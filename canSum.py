482d569c355b20322e0e67327f27ad1198ff95c7
1orwell <elise@eliseratcliffe.co.uk>
2022-03-22 14:58:23 +0000

    I think this solution of canSum works?

---
+++ canSum.py
@@ -0,0 +1,21 @@
+#This should check if we can sum some combination of numbers in the list to reach the target sum.
+def canSum(targetSum, numbers, memo):
+    if targetSum == 0: # base case - exact amount reached, therefore possible
+        return True
+    if targetSum < 0: # base case - subtracted too much so that combo doesn't work
+        memo[targetSum] = False
+        return False
+    
+    if targetSum in memo: # 'base case' for memoization
+        return memo[targetSum]
+
+    for num in numbers:
+        remainder = targetSum - num
+        # if any of the child nodes return true, there is a path to 0, so true
+        if canSum(remainder, numbers, memo) == True:
+            memo[remainder] = True
+            return True
+
+
+boolean = canSum(100, [5, 3, 4, 7], {})
+print(boolean)
