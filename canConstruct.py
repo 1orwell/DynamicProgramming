2d316546a1b3098bf84d757e8a9381dbe85ccb07
1orwell <elise@eliseratcliffe.co.uk>
2022-03-23 08:49:58 +0000

    working on canConstruct, the logic seems fairly right but I think I must not be setting the boolean to True in the right place.

---
+++ canConstruct.py
@@ -0,0 +1,58 @@
+#work in progress
+#returning False when should return True
+
+def canConstruct(target, wordBank, possible):
+
+    # if we have reached a successful base case, return True
+    if target == "":
+        possible = True
+        return possible
+
+    first_letter_check = False
+
+    #check the first letter of each word in the word bank
+    #if none of these match the first letter of the target
+    #return False
+    for word in wordBank:
+        first_letter = word[0]
+        if first_letter == target[0]:
+            first_letter_check = True
+    if first_letter_check == False:
+        possible = False
+        return possible
+
+    for word in wordBank:
+        match = True
+        target_len = len(target)
+        word_len = len(word)
+        #if the word in the word bank is longer than the target
+        # move onto the next word
+        if word_len > target_len:
+            continue
+        #iterate through each character in the word from the word bank
+        for index in range(0, word_len):
+            #if the corresponding characters do no match
+            #set match to False
+            if target[index] != word[index]:
+                match = False
+        # if match never gets set to False, that means word is a subset of target
+        if match == True:
+            #remove word from beginning of target
+            possible = True
+            target = target[word_len:]
+            possible = canConstruct(target, wordBank, possible)
+        
+        if possible == False:
+            return possible
+    
+    return possible
+        
+    
+
+    
+
+
+
+
+result = canConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"], False)
+print(result)
