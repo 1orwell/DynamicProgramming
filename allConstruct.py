754c225bc88a35789e2834dadd12139a63eb6d47
1orwell <elise@eliseratcliffe.co.uk>
2022-03-23 15:54:36 +0000

    mapping abandoned as insert returned None, loop used

---
+++ allConstruct.py
@@ -0,0 +1,46 @@
+def add_prefix(ways, word):
+    ways.insert(0, word)
+    return ways
+
+result = []
+
+def allConstruct(target, wordBank):
+    if target == "":
+        return([[]])
+
+    for word in wordBank:
+        match = True
+        target_len = len(target)
+        word_len = len(word)
+        #if the word in the word bank is longer than the target
+        # move onto the next word
+        if word_len > target_len:
+            continue
+        #iterate through each character in the word from the word bank
+        for index in range(0, word_len):
+            #if the corresponding characters do no match
+            #set match to False
+            if target[index] != word[index]:
+                match = False
+        # if match never gets set to False, that means word is a subset of target
+        if match == True:
+            #remove word from beginning of target
+            new_target = target[word_len:]
+            suffix_ways = allConstruct(new_target, wordBank)
+            #target_ways = map(lambda x: x.insert(0, word), suffix_ways)
+            # the above line didn't work as insert does not return a value
+            for way in suffix_ways:
+                way.insert(0, word) 
+            print("target ways are: ")
+            for way in suffix_ways:
+                print(way)
+            # "spread out" the suffix_ways so we keep a 2D array
+            result.append(suffix_ways)
+
+    return result
+
+
+#result = allConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"])
+#print(result)
+result = allConstruct("abc", ["ab", "c", "cd", "abc"])
+print(result)
---
+++ mapTest.py
@@ -0,0 +1,14 @@
+twod_list = [[4,5,6,7,8], [6,5,3], [4,5,1,2]]
+simple_list = [4,5,6,7,8]
+print(twod_list)
+new_map = map(lambda x: x.append(10), twod_list)
+simple_new_map = map(lambda x: x+1, simple_list)
+new_list = list(new_map)
+simple_new_list = list(simple_new_map)
+
+for val in new_list:
+    print(val)
+
+for val in simple_new_list:
+    print(val)
+
